5509b5969c6423769c6b3ca6aa3a4e710ff4bd15
mithunbabbira <mithunbabbira@gmail.com>
2019-11-18 00:59:49 +0530

    update

--- app/src/main/java/com/example/samsdebug/Main2Activity.java
+++ app/src/main/java/com/example/samsdebug/Main2Activity.java
@@ -35,6 +35,7 @@ public class Main2Activity extends AppCompatActivity {
 
 
 
+
         String url = "https://script.googleusercontent.com/macros/echo?user_content_key=mtiRBNZPjH8LlBhIAaXcFF-tbGkl_Tke6KDH4s2gtlq3_8yGTeCMsqmW_crt2IWVA5HfiAT4RwySp1awpvxJm6CZ--bpMFrHOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1GhPSVukpSQTydEwAEXFXgt_wltjJcH3XHUaaPC1fv5o9XyvOto09QuWI89K6KjOu0SP2F-BdwUXbwfl4vKUFagc_f4QNM6dR4Lj4t8CH39sCVO2ZGMyEH3aAJF6OGQ8ogXUTGdbFrG5y7FLqOV0Tk27B8Rh4QJTQ&lib=MnrE7b2I2PjfH799VodkCPiQjIVyBAxva";
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
